6e924d3ae46856f1e398489cfac94cfcebe84fda
涵先森Eli <41312552+HanxiansenEli@users.noreply.github.com>
2019-07-31 12:27:57 +0800

    Create AMGraph_BFS.cpp

---
+++ AMGraph_BFS.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <queue>
+#include "AMGraph_Create.cpp"
+
+void BFS(AMGraph & G, int v)
+{
+	int distance[MAX_SIZE]; // 路径距离
+	queue<int> que;
+
+	for(int i = 0; i < G.vexnum; i++)
+		distance[i] = INF;
+
+	que.push(v);
+	distance[v] = 0;
+	cout << "Path:"<< v << " ";	// 输出起始点 
+
+	while(!que.empty()){
+		int cur = que.front();
+		que.pop();
+
+		for(int k = 0; k < G.vexnum; k++){
+				if(G.arcs[cur][k] != INF && distance[k] == INF){
+					cout << k << " ";
+					distance[k] = distance[cur] + 1;
+					que.push(k);
+				}
+			}
+		}
+
+	cout << endl << "Min_distance:";
+	for(int i = 0; i < G.vexnum; i++)
+		cout << distance[i] << " ";
+}
+
+void solve()
+{
+	AMGraph G;
+	CreateUDN(G);
+	BFS(G, 0);
+}
