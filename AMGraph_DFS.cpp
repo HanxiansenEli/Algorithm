3625cfdf60ae6976da06caddf728ee57d15ffb60
涵先森Eli <41312552+HanxiansenEli@users.noreply.github.com>
2019-07-31 12:32:15 +0800

    Create AMGraph_DFS.cpp

---
+++ AMGraph_DFS.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include "AMGraph_Create.cpp"
+
+void DFS(AMGraph & G, bool * flag, int v)
+{
+	cout << v << " ";
+	flag[v] = true;
+	for(int k = 0; k < G.vexnum; k++){
+		if(!flag[k] && G.arcs[v][k] != INF){
+			DFS(G, flag, k);
+		}
+	}
+}
+
+void solve()
+{
+	AMGraph G;
+	CreateUDN(G);
+	bool flag[MAX_SIZE];
+	for(int i = 0; i < G.vexnum; i++)
+		flag[i] = false;
+	DFS(G, flag, 0);
+}
+
+int main()
+{
+	solve();
+}
